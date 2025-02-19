1d73a6fa93fdfaaab383ec9dbb30d98c970ec1a6
kingsawpdx <kingsaw@pdx.edu>
2025-02-18 21:48:17 -0800

    Created backend and /GET support for words

---
+++ api/api.js
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import process from "process";
+import dotenv from "dotenv";
+
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config({ path: "../.env" });
+
+const API_KEY = process.env.API_KEY;
+const PROJECT_URL = process.env.PROJECT_URL;
+
+const app = express();
+
+app.use(morgan("combined"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const supabase = createClient(PROJECT_URL, API_KEY);
+
+app.get("/words", async (req, res) => {
+  const { data, error } = await supabase.from("/words").select();
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log(`> Ready on http://localhost:3000`);
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then((res) => res.json())
+      .then((data) => setWords(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
       <h1>SpeakEasy</h1>
+      <ul>
+        {words.map((word, index) => (
+          <li key={index}>{word.word}</li>
+        ))}
+      </ul>
     </>
   );
 }
