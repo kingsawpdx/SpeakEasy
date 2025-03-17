fda6c4e942e9ebd28d2bee7be258f70b8031f9cb
MarsTL <165991919+MarsTL@users.noreply.github.com>
2025-03-16 19:00:35 -0700

    more style change

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -11,16 +11,21 @@ const SideBar = ({ handlePlay }) => {
     { id: 6, word: "Please" },
     { id: 7, word: "Yes" },
     { id: 8, word: "No" },
+    { id: 9, word: "I feel" },
+    { id: 10, word: "Good Bye" },
+    { id: 11, word: "No problem" },
+    { id: 12, word: "I'm sorry" },
+    { id: 13, word: "I wait for" },
   ];
 
   return (
-    <div className="sidebar fixed-left p-4" style={{ width: '180px', background: 'lightblue', height: '100vh', overflowY: 'auto', marginTop:'170px'}}>
+    <div className="sidebar fixed-left p-4" style={{ width: '195px', background: 'lightblue', height: '100vh', overflowY: 'auto', marginTop:'170px'}}>
       <h5 className="text-center mb-4">Common Words</h5>
       <div className="d-flex flex-column " >
         {commonWords.map((item) => (
           <button
             key={item.id}
-            className="btn btn-light mb-1 sidebar-button"
+            className="btn btn-light mb-2 sidebar-button"
             onClick={() => handlePlay(item.word)}
             style={{ border: '2px solid black', borderRadius: '10px', padding: '10px' }}
           >
